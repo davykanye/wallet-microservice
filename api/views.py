b66ebd4d793efafcf03627592fa3fdb8e3b7527c
davykanye <74606340+davykanye@users.noreply.github.com>
2022-12-05 14:34:11 +0100

    project setup

---
+++ api/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+'''
+------- WALLETS ---------------
+- CRUD wallet
+- Transfer || wallet -> wallet
+- Fund wallet account -> wallet
+- Transfer || wallet -> account
+-------- TRANSACTIONS ---------------
+- CRUD Transaction
+- fetch all transactions
+- fetch user transactions
+
+'''
---
+++ core/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+# NOTE: Write robust models and lots of methods
+'''
+--------- MODELS ----------------
+'''
