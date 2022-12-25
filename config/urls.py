667b2bdb69f21eeba8d8a8b46c0a2f7c03cebabd
davykanye <davykanye@gmail.com>
2022-12-25 22:21:33 +0100

    collectstatic changes

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -46,4 +49,4 @@ urlpatterns = [
                                          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
                                        cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
